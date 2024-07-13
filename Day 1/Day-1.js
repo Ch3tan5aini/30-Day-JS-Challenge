599ecabb48e20ceb4dfbf52ba905a6dbda4a7372
chetan saini <ch3tan5aini@gmail.com>
2024-07-13 19:49:54 +0530

    Day 1 complete

---
+++ Day 1/Day-1.js
@@ -0,0 +1,29 @@
+// Variable Assignment
+var num = 9;
+console.log(num);
+
+// let Assignment
+let text = "Hello World";
+console.log(text);
+
+// const Assignment
+const check = true;
+console.log(check);
+
+// Data types and use of type of 
+const string = "Hello World";
+const number = 55;
+const boolean = false;
+const object = {name:"Chetan"};
+const array = [1,2,"hello"];
+console.log(typeof string );
+console.log(typeof number );
+console.log(typeof boolean );
+console.log(typeof object );
+console.log(Array.isArray(array)); // array is a special type of object 
+
+// reassignment of value to a variable
+text = "Hello World Again"
+console.log(text);
+
+// check = true;  const variable cannot be changed after assignment
